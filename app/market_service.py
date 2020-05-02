4bbc3bd9af98cae4ce5f9fd5e733b5875c008921
Alexa Dolby <59667085+alexadolby@users.noreply.github.com>
2020-05-02 17:15:34 -0400

    market service

--- app/market_service.py
+++ app/market_service.py
@@ -9,4 +9,45 @@ from app import APP_ENV
 
 load_dotenv()
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
 ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+
+symbol = input("Please input a stock symbol, when finished type DONE: ")
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={ALPHA_VANTAGE_API_KEY}"
+response = requests.get(request_url)
+parsed_response = json.loads(response.text)
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+tsd = parsed_response["Time Series (Daily)"]
+
+dates = list(tsd.keys())
+
+latest_day = dates[0]
+
+
+latest_close = tsd[latest_day]["4. close"]
+high_prices = []
+low_prices = []
+
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    low_price = tsd[date]["3. low"]
+    high_prices.append(float(high_price))
+    low_prices.append(float(low_price))
+
+recent_high = max(high_prices)
+recent_low = min(low_prices)
+
+print("-------------------------")
+print(f"SELECTED SYMBOL: {symbol}")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("-------------------------")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
